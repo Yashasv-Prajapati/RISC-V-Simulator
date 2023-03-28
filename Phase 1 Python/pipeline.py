e822ceab5752df7aaba93bb116a1a3c3bdbaa4d0
yashasav <yamuprajapati05@gmail.com>
2023-03-28 23:04:24 +0530

    New pipeline file

---
+++ Phase 1 Python/pipeline.py
@@ -0,0 +1,34 @@
+import os
+
+# creating 4 dictionaries for each stage
+# dictionary for the first stage
+stage1 = {}
+
+# dictionary for the second stage
+stage2 = {}
+
+# dictionary for the third stage
+stage3 = {}
+
+# dictionary for the fourth stage
+stage4 = {}
+
+registerCheck = [1 for i in range(32)]
+
+# functions to be made----> fetch, decode, execute, memory, writeback
+
+# function to fetch the instruction from the instruction memory
+def fetch():
+    print('fetch')
+
+def decode():
+    print('decode')
+
+def execute():
+    print('execute')
+
+def memory():
+    print('memory')
+
+def writeback():
+    print('writeback')
