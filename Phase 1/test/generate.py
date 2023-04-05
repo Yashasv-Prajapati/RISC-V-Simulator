0b83cbd1ce92a3d99391a0b8cd6c8bcd6eef4aa3
mathnebula186f <getgopalbansal@gmail.com>
2023-04-06 00:14:48 +0530

    Major Changes done and organized code

--- Phase 1/test/generate.py
+++ Phase 1/test/generate.py
@@ -1,7 +1,7 @@
 i=0
 output = open('output2.mem', 'w')
 
-with open('bubble_sort.mem', 'r') as f:
+with open('newtest.mem', 'r') as f:
     for line in f:
         text = hex(i)+' 0x'+line
         output.write(text)
