5812f023ebb8579b3010ada81a3f3867722f5249
mathnebula186f <getgopalbansal@gmail.com>
2023-04-25 23:12:57 +0530

    JSON files generated and stats done printleft

---
+++ GUI Files/TestFiles/generate.py
@@ -0,0 +1,13 @@
+i=0
+output = open('output.mem', 'w')
+
+with open('input.mem', 'r') as f:
+    for line in f:
+        text = hex(i)+' 0x'+line
+        output.write(text)
+        i += 4
+    text = hex(i) + ' 0x00000033\n'
+    output.write(text)
+    i+=4
+    text=hex(i) + ' 0xfffffffb'
+    output.write(text)
