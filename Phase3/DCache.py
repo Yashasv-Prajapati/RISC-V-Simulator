92d683c7e27bb84791a0062f765ceb0c44d0a1b7
yashasav <yamuprajapati05@gmail.com>
2023-04-25 22:21:18 +0530

    policies implemented and datafrom main memory function implemented

--- Phase3/DCache.py
+++ Phase3/DCache.py
@@ -2,6 +2,8 @@
 import math
 import numpy as np
 
+data_mem = np.zeros((2**32), dtype='uint8') # main memory
+
 class DCache:
     __policies = ["LRU", "FIFO", "Random", "LFU"]
     __mapping = ["direct", "set-associative", "fully-associative"]
@@ -21,34 +23,38 @@ class DCache:
         self.capacity_misses = 0
         self.conflict_misses = 0
 
-
-        # LRU policy
-        # number of ways x 3, 3 is for the Valid bit, status bit and tag bit
-        self.LRU = np.zeros((numberOfWays), dtype='uint32')
-
-        # FIFO policy
-        self.FIFO = np.zeros((numberOfWays), dtype='uint32')
-
-        # LFU policy
-        self.LFU = np.zeros((numberOfWays,2), dtype='uint32')
-
-        # Random policy
-        self.Random = np.zeros((numberOfWays), dtype='uint32')
-
-        self.cache_access = {} # dictionary to store all the cache accesses
-        self.mct = {} # miss classification table
-
         # set number_of_ways
         self.setNumberOfWays(numberOfWays, mapping, cacheSize, blockSize)
 
         # set replacement policy
         self.setReplacementPolicy(policyName)
 
+        
+
+
+        # LRU policy
+        # number of ways x 3, 3 is for the Valid bit, status bit and tag bit
+        self.LRU = np.zeros((self.number_of_ways), dtype='uint32')
+
+        # FIFO policy
+        self.FIFO = np.zeros((self.number_of_ways), dtype='uint32')
+
+        # LFU policy
+        # self.LFU = np.zeros((numberOfWays,2), dtype='uint32')
+        self.LFU = {}
+
+        # Random policy
+        self.Random = np.zeros((self.number_of_ways), dtype='uint32')
+
+        self.cache_access = {} # dictionary to store all the cache accesses
+        self.mct = {} # miss classification table
+
+
         # total tag arrays -> number of ways and each array is a dictionary 
-        self.tag_arrays = np.full((numberOfWays), {}, dtype=object)
+        self.tag_arrays = np.full((self.number_of_ways), {}, dtype=object)
 
         # number of data arrays = number of ways. each array has <number of blocks> rows and <block size> columns
-        self.data_cache = np.full((numberOfWays, cacheSize//blockSize, blockSize), 0, dtype='uint8')
+        self.data_cache = np.full((self.number_of_ways, cacheSize//blockSize, blockSize), 0, dtype='uint8')
 
     def setNumberOfWays(self, numberOfWays:int, mapping:str, cacheSize:int, blockSize:int):
         if(mapping not in self.__mapping):
@@ -59,7 +65,7 @@ class DCache:
         elif mapping == "set-associative":
             self.number_of_ways = numberOfWays
         elif mapping == "fully-associative":
-            self.number_of_ways = cacheSize/blockSize # total Blocks
+            self.number_of_ways = cacheSize//blockSize # total Blocks
         
     def setReplacementPolicy(self, policyName:str):
         if(policyName not in self.__policies):
@@ -92,40 +98,62 @@ class DCache:
             self.tag_arrays[wayNumber][Index] = Tag
 
             # get data from main memory
-            dataFromMain = self.getFromMain(address)
+            binaryDataFromMain = self.getFromMain(address)
             
             # set data in cache  
             # converting dataReceived to a 256 bit binary string
-            dataInBinary = bin(dataFromMain)[2:].zfill(256) # data of form '100010001...' -> 256 bits
+            for i in range(self.block_size):
+                self.data_cache[wayNumber][Index][i] = int(binaryDataFromMain[i*8:i*8+8],2)
             
-            # now storing each byte in the cache
-            for i in range(32):
-                self.data_cache[wayNumber][Index][i] = int(dataInBinary[i*8:i*8+8],2)
-
+            print("Data in cache: ", self.data_cache[wayNumber][Index])
+            
             return self.data_cache[wayNumber][Index][blockOffset]
+
+    def WriteDataInMain(self, address:int, data:int):
+        '''
+        This function is used to set data in the main memory
+        Considering data to be of 32 bits/4 Bytes or max value of int to be 2^31 - 1
+        '''
+        dataInBinary = bin(data)[2:].zfill(32) # converting data to a 32 bit binary string
+        # set data in main memory
+        for i in range(4):
+            data_mem[address+i] = int(dataInBinary[i*8:i*8+8],2)
+        
+       
 
     def getFromMain(self, address:int):
         '''
         This function is used to get data from the main memory
-        '''
-        # return data from main memory, considering main memory to be a dictionary
-        return data_mem.get(address, 0)
-        
-    def data(self, address:int, func3:int):
-        '''
-        This function is used to set data from the cache
-        It will set it to the main memory 
 
         address: the address of the data
-        func3: depending on type of store data
-            if func3 == 0b000 -> sb
-            if func3 == 0b001 -> sh
-            if func3 == 0b010 -> sw
+
+        returns: binary string of data of size `blocksize` from main memory
         '''
-        # add data here
+        # return data from main memory, considering main memory to be a dictionary
+        # we have to take blocksize amount of bytes from main memory
+        dataFromMain = ''
+        for i in range(self.block_size):
+            dataFromMain += bin(data_mem[address+i])[2:].zfill(8) # taking 8 bits/1byte at a time 
+        
     
-    def LRU():
-        pass
+        return dataFromMain        
+    
+    def write_data(self, address:int, data:int, func3:int):
+        '''
+        This function is used to write data in the cache
+        first the data is written in the cache then subsequently in the main memory at that particular address
+
+        address: the address of the data
+        data: the data to be written
+        func3: depending on type of store data
+            if func3 == 0b000 / 0 -> sb
+            if func3 == 0b001 / 1 -> sh
+            if func3 == 0b010 / 2 -> sw
+        '''
+
+        Tag, Index, blockOffset = self.break_address(address)
+
+        
     
     def get_data(self,address:int, func3:int):
 
@@ -159,22 +187,23 @@ class DCache:
                     self.conflict_misses += 1
                 else: # capacity miss
                     self.capacity_misses += 1
-            
-            # else the data is present in the cache -> hit
-            self.hits += 1
+            else:
+                # else the data is present in the cache -> hit
+                self.hits += 1
+
             # check type of load instruction
             if(func3==0): # lb
                 return self.readByte(address, 0)
             
             elif(func3==1): # lh
                 for i in range(2):
-                    DataFound.append(bin(self.readByte(address, 0))[2:].zfill(8)) # convert to binary and pad with zeros
+                    DataFound += bin(self.readByte(address, 0))[2:].zfill(8) # convert to binary and pad with zeros
                     address+=1 # increment address by 1 to get new address
                 return int(DataFound,2)
             
             elif(func3==2): # lw
                 for i in range(4):
-                    DataFound.append(bin(self.readByte(address, 0))[2:].zfill(8)) # convert to binary and pad with zeros
+                    DataFound += bin(self.readByte(address, 0))[2:].zfill(8) # convert to binary and pad with zeros
                     address += 1 # increment address by 1 to get new address
                 return int(DataFound,2)
             
@@ -217,17 +246,19 @@ class DCache:
                     wayNum = LRUWay # way number where data is to be replaced from and return the updated data
                     self.LRU.insert(0,LRUWay) # insert it at the start to make it the most recently used
                 elif(self.replacement_policy=='FIFO'):
-                    pass
                     FIFOWay = self.FIFO.pop(0) # get the first entry in the queue
                     wayNum = FIFOWay
                     self.FIFO.append(FIFOWay) # append it to the end of the queue
                 elif(self.replacement_policy=='random'):
                     pass
                     # apply the easiet policy here
+                    
                 elif(self.replacement_policy=='LFU'):
-                    pass
-
-
+                    # apply the LFU policy here
+                    MIN = min(self.LFU, key=self.LFU.get) # get the key with the maximum value
+                    wayNum = MIN # way number where data is to be replaced from and return the updated data
+                    self.LFU[MIN] += 1 # increment the value of the key
+                
                 if(Tag in self.mct):
                     self.conflict_misses += 1
                 else:
@@ -240,57 +271,133 @@ class DCache:
             
             elif(func3==1): # lh
                 for _ in range(2):
-                    DataFound.append(bin(self.readByte(address, wayNum))[2:].zfill(8)) # convert to binary and pad with zeros
+                    DataFound += bin(self.readByte(address, wayNum))[2:].zfill(8) # convert to binary and pad with zeros
                     address+=1 # increment address by 1 to get new address
                 return int(DataFound,2)
             
             elif(func3==2): # lw
                 for _ in range(4):
-                    DataFound.append(bin(self.readByte(address, wayNum))[2:].zfill(8)) # convert to binary and pad with zeros
+                    DataFound += bin(self.readByte(address, wayNum))[2:].zfill(8) # convert to binary and pad with zeros
                     address += 1 # increment address by 1 to get new address
                 return int(DataFound,2)
             else:
-                raise ValueError("Invalid func3 value, expected 0,1 or 2 but got "+str(func3)+" instead")
+                raise ValueError("Invalid func3 value, expected 0, 1 or 2 but got "+str(func3)+" instead")
 
         
         elif self.mapping == "fully-associative":
+            # check type of miss
+            ColdMiss = True
+            CapacityORConflict = False
+            wayNum = None
+
+            # look into all the ways to find the data
             for i in range(len(self.tag_arrays)):
-                if self.tag_arrays[i].get(Index,-1) == Tag: # found in cache
-                    self.hits += 1
-                    return self.blocks[i][Index]
+                if(self.tag_arrays[i].get(Index, -1)==Tag): # hit
+                    ColdMiss = False
+                    CapacityORConflict = False
+                    wayNum = i # way number where data is found
+                    self.hits =+ 1 # increment hits
+                    break
+                if(self.tag_arrays[i].get(Index, -1)==-1): # cold miss
+                    ColdMiss = True # not a cold miss
+                    CapacityORConflict = False # not a capacity miss
+                    wayNum = i # way number where data is to be stored
+                    break
+                else: # either capacity miss or conflict miss
+                    CapacityORConflict = True
+
+            # ---------------------------------------------------------------
+            if(ColdMiss): # if cold miss
+                self.cold_misses += 1
+            elif(CapacityORConflict): # either conflict miss or capacity miss
+                
+                # check if replacement policy is valid
+                if(self.replacement_policy not in self.__policies):
+                    raise ValueError("Invalid replacement policy")
+                
+                # evict according to given policy
+                if(self.replacement_policy=="LRU"):
+                    LRUWay = self.LRU.pop(-1) # get the least recently used way
+                    wayNum = LRUWay # way number where data is to be replaced from and return the updated data
+                    self.LRU.insert(0,LRUWay) # insert it at the start to make it the most recently used
+                elif(self.replacement_policy=='FIFO'):
+                    FIFOWay = self.FIFO.pop(0) # get the first entry in the queue
+                    wayNum = FIFOWay
+                    self.FIFO.append(FIFOWay) # append it to the end of the queue
+                elif(self.replacement_policy=='random'):
+                    pass
+                    # apply the easiet policy here
+                    
+                elif(self.replacement_policy=='LFU'):
+                    # apply the LFU policy here
+                    MIN = min(self.LFU, key=self.LFU.get) # get the key with the maximum value
+                    wayNum = MIN # way number where data is to be replaced from and return the updated data
+                    self.LFU[MIN] += 1 # increment the value of the key
+                
+                if(Tag in self.mct):
+                    self.conflict_misses += 1
+                else:
+                    self.capacity_misses += 1
+            # ---------------------------------------------------------------
+            # once you know the wayNumber, now go to that way and get data from there
+            # check type of load instruction
+            if(func3==0): # lb
+                return self.readByte(address, wayNum)
             
-            self.misses += 1
+            elif(func3==1): # lh
+                for _ in range(2):
+                    DataFound += bin(self.readByte(address, wayNum))[2:].zfill(8) # convert to binary and pad with zeros
+                    address+=1 # increment address by 1 to get new address
+                return int(DataFound,2)
+            
+            elif(func3==2): # lw
+                for _ in range(4):
+                    DataFound += bin(self.readByte(address, wayNum))[2:].zfill(8) # convert to binary and pad with zeros
+                    address += 1 # increment address by 1 to get new address
+                return int(DataFound,2)
+            else:
+                raise ValueError("Invalid func3 value, expected 0, 1 or 2 but got "+str(func3)+" instead")
+
+        
+
             return None
 
     def break_address(self, address:int):
         # considering 32 bit address
         address = bin(address)[2:].zfill(32) # converting to binary and padding with zeros to make it 32 bits
-        
         numberOfBlocks = self.cache_size / self.block_size
         numberOfSets = numberOfBlocks / self.number_of_ways
         
         # Calculating the number of bits for tag, index and block offset
-        IndexBitsNum = math.log(numberOfSets, 2)
-        blockOffsetBitsNum = math.log(self.block_size, 2)
-        TagBitsNum = 32 - IndexBitsNum - blockOffsetBitsNum # remaining bits are tag bits
+        IndexBitsNum = int(math.log(numberOfSets, 2))
+        blockOffsetBitsNum = int(math.log(self.block_size, 2))
+        TagBitsNum = int(32 - IndexBitsNum - blockOffsetBitsNum) # remaining bits are tag bits
 
         # Calculating the tag, index and block offset
-        Tag = int(address[0:TagBitsNum],2)
-        Index = int(address[TagBitsNum:IndexBitsNum + TagBitsNum],2)
-        blockOffset = (address[TagBitsNum+IndexBitsNum:TagBitsNum+IndexBitsNum+blockOffsetBitsNum],2)
+        if(address[0:TagBitsNum]==""):
+            Tag = 0
+        else:
+            Tag = int(address[0:TagBitsNum],2)
+        
+        if(address[TagBitsNum:IndexBitsNum + TagBitsNum]==""):
+            Index = 0
+        else:
+            Index = int(address[TagBitsNum:IndexBitsNum + TagBitsNum],2)
+        
+        if(address[TagBitsNum+IndexBitsNum:TagBitsNum+IndexBitsNum+blockOffsetBitsNum]==""):
+            blockOffset = 0  
+        else:
+            blockOffset = int(address[TagBitsNum+IndexBitsNum:TagBitsNum+IndexBitsNum+blockOffsetBitsNum],2)
 
         return Tag, Index, blockOffset
     
 
+if __name__ =='__main__':
+    L1 = DCache(32, 32, "direct", "LRU", 0)
+    L1.WriteDataInMain(0, 2**31-1)
 
-
-
-
-
-
-
-
-
+    data = L1.get_data(0, 2)
+    print(data)
 
 
 
